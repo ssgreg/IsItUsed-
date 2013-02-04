a11f1d95f5eea75cf72d7a1255ff60bd23cf45fa
Grigory Zubankov <ss.greg@me.com>
2013-02-04 14:21:26 +0400

    stream and shared schemes

---
+++ RunningProcessesInfo/RunningProcessesInfo/src/JetMemoryStream.h
@@ -0,0 +1,12 @@
+//
+// Created by igreg on 11/26/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface JetMemoryStream : NSObject
+@end
---
+++ RunningProcessesInfo/RunningProcessesInfo/src/JetMemoryStream.m
@@ -0,0 +1,15 @@
+//
+// Created by igreg on 11/26/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "JetMemoryStream.h"
+
+
+@implementation JetMemoryStream {
+
+}
+
+@end
